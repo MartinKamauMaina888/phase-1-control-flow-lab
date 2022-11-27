129a3d9cde257178b224f1aa1f12499956a23f2d
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-27 11:11:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+  if (distance <= 400)
+  {
+    return "This one is on me!";
+  }
+
+else if (distance > 2000 && Figure < 2500)
+{
+return "I will gladly take your thirty bucks.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if(distance > 2501)
+{
+return "No can do.";
+}
 }
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+  if (city === "NYC") ? "ok, sounds good" : "No go";
+
+
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === "generous") 
+  {
+    return "Thank you so much.";
+  } 
+
+else if (tip === "not generous") 
+  {
+    return "Thank you.";
+  } 
+else (null)
+  {
+    return "Bye.";  
+  }
 }
